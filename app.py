ddcf22e6f46a93413eb8fb6ad3721630c9e58c2a
Margot Sauter <margot@smarttrackers.nl>
2023-10-11 11:39:02 +0200

    Add form to submit From and To station

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import locale
 import requests
 
@@ -9,29 +9,35 @@ locale.setlocale(locale.LC_ALL, 'nl_NL')
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # Get price from nijmegen to utrecht from ns api
-    url = 'https://gateway.apiportal.ns.nl/public-prijsinformatie/prices?fromStation=Nijmegen&toStation=Utrecht'
-    headers = {'Ocp-Apim-Subscription-Key': os.environ.get('PUBLIC_PRICE_INFORMATION_KEY')}
-    response = requests.get(url, headers=headers)
-    data = response.json()
+    if request.method == "POST":
+        fromStation = request.form.get("fromStation")
+        toStation = request.form.get("toStation")
 
-    # Select only the prices for a single fare in second class
-    prices = []
-    for price_option in data['priceOptions']:
-        if price_option['type'] == 'ROUTE_WITHOUT_OPTIONS':
-            for total_price in price_option['totalPrices']:
-                if total_price['classType'] == 'SECOND' and total_price['productType'] == 'SINGLE_FARE':
-                    prices.append(total_price)
+        # Get prices from fromStation to toStation from NS API
+        url = "https://gateway.apiportal.ns.nl/public-prijsinformatie/prices?fromStation={}&toStation={}".format(fromStation, toStation)
+        headers = {'Ocp-Apim-Subscription-Key': os.environ.get('PUBLIC_PRICE_INFORMATION_KEY')}
+        response = requests.get(url, headers=headers)
+        data = response.json()
 
-    # Sort by price, lowest first
-    prices = sorted(prices, key=lambda k: k['price'])
+        # Select only the prices for a single fare in second class
+        prices = []
+        for price_option in data['priceOptions']:
+            if price_option['type'] == 'ROUTE_WITHOUT_OPTIONS':
+                for total_price in price_option['totalPrices']:
+                    if total_price['classType'] == 'SECOND' and total_price['productType'] == 'SINGLE_FARE':
+                        prices.append(total_price)
 
-    # Format price in euros and discount as text
-    for price in prices:
-        price['price'] = locale.currency(price['price'] / 100, grouping=True).replace('Eu', '€')
-        price['discount'] = price['discountType'].replace('_', ' ').capitalize()
+        # Sort by price, lowest first
+        prices = sorted(prices, key=lambda k: k['price'])
 
-    return render_template('index.html', prices=prices)
+        # Format price in euros and discount as text
+        for price in prices:
+            price['price'] = locale.currency(price['price'] / 100, grouping=True).replace('Eu', '€')
+            price['discount'] = price['discountType'].replace('_', ' ').capitalize()
 
+        return render_template('index.html', prices=prices, fromStation=fromStation, toStation=toStation)
+
+    else:
+        return render_template('index.html')
